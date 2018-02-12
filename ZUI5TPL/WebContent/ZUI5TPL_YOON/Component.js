08a878f08861d64428c363e30bd9f3555e5736ac
echoit <echoit@echoit-PC>
2018-02-12 14:29:04 +0900

    ff

--- ZUI5TPL/WebContent/ZUI5TPL_YOON/Component.js
+++ ZUI5TPL/WebContent/ZUI5TPL_YOON/Component.js
@@ -12,8 +12,8 @@ sap.ui.define([
          // call the init function of the parent
          UIComponent.prototype.init.apply(this, arguments);
          
-         // create the views based on the url/hash
-         this.getRouter().initialize();
-	}
+//          create the views based on the url/hash
+         this.getRouter();
+      }
    });
 });
