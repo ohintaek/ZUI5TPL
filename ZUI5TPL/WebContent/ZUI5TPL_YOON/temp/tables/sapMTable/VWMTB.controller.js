f15c8a37aa44df3f0bc9e387da088f8c4709d0ee
yoon <yoon@yoon-PC>
2018-02-17 11:26:51 +0900

    sap.m.table page update

--- ZUI5TPL/WebContent/ZUI5TPL_YOON/temp/tables/sapMTable/VWMTB.controller.js
+++ ZUI5TPL/WebContent/ZUI5TPL_YOON/temp/tables/sapMTable/VWMTB.controller.js
@@ -1,8 +1,9 @@
 sap.ui.define([
 	"com/ui5/yoon/controller/BaseController",
 	"sap/ui/core/routing/History",
+	"sap/ui/model/json/JSONModel",
 	"sap/m/MessageToast"
-], function (Controller, History, MessageToast) {
+], function (Controller, History, JSONModel, MessageToast) {
 	"use strict";
 
 	return Controller.extend("com.ui5.yoon.temp.tables.sapMTable.VWMTB", {
@@ -13,7 +14,16 @@ sap.ui.define([
 		},
 		
 		onRouteMatched : function(oEvent) {
-			var oSource = oEvent.getSource();
+			var jsonModel = new JSONModel(jQuery.sap.getModulePath("com/ui5", "/models/customers.json"));
+				jsonModel.attachRequestCompleted(function(oEvent){  
+					var ModelNEW = oEvent.getSource();
+				});
+				
+//			jsonModel.loadData("com/ui5/models/customers.json");
+//			jsonModel.loadData("../../../../models/customers.json");
+			
+			var oTable = this.getView().byId("mtable");
+			
 		},
 		
 		onNavBack: function() {
