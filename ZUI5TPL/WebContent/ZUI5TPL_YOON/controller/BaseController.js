b973d82196b3cbb0cf70aa2fe00ff55698b9e8be
echoit <echoit@activation.acronis.com>
2018-02-19 17:00:35 +0900

    2018/02/19 release (by yoon)

--- ZUI5TPL/WebContent/ZUI5TPL_YOON/controller/BaseController.js
+++ ZUI5TPL/WebContent/ZUI5TPL_YOON/controller/BaseController.js
@@ -46,6 +46,16 @@ sap.ui.define([
 					this.getRouter().navTo("Home");
 				}
 			},
+			
+			getOdataServiceUrl : function() {
+				  //for local testing prefix with proxy
+				  //if you and your team use a special host name or IP like 127.0.0.1 for localhost please adapt the if statement below 
+				  var sServiceUrl = "http/ECHOSAP1.echoit.co.kr:8010" + jQuery.sap.getModulePath("odataservice", "/");
+				  if (window.location.hostname == "localhost") {
+				      return "proxy/" + sServiceUrl;
+				  } else {
+				      return sServiceUrl;
+				  }
+			}
 		});
-
 	});
--- ZUI5TPL/WebContent/ZUI5TPL_YOON/temp/tables/sapMTable/VWMTB.controller.js
+++ ZUI5TPL/WebContent/ZUI5TPL_YOON/temp/tables/sapMTable/VWMTB.controller.js
@@ -1,10 +1,12 @@
 sap.ui.define([
 	"jquery.sap.global",
 	"com/ui5/yoon/controller/BaseController",
+	"com/ui5/yoon/controller/CommonUtil",
+	"sap/ui/model/odata/ODataModel",
 	"sap/ui/core/routing/History",
 	"sap/ui/model/json/JSONModel",
 	"sap/m/MessageToast"
-], function (jQuery, Controller, History, JSONModel, MessageToast) {
+], function (jQuery, Controller, CommonUtil, ODataModel, History, JSONModel, MessageToast) {
 	"use strict";
 
 	return Controller.extend("com.ui5.yoon.temp.tables.sapMTable.VWMTB", {
@@ -17,9 +19,8 @@ sap.ui.define([
 		onRouteMatched : function(oEvent) {
 			var jsonModel = new JSONModel(jQuery.sap.getModulePath("com.ui5.yoon.models", "/customers.json"));
 			this.getView().setModel(jsonModel);
-			
-			var date = '10.02.2018';
-			var oDate = new Date(date);
+		
+			var oModel = new ODataModel(CommonUtil.getOdataServiceUrl(), true);
 			
 			var oTable = this.getView().byId("mtable");
 			oTable.setModel(jsonModel);
@@ -29,22 +30,22 @@ sap.ui.define([
 					cells : [
 						new sap.m.Text({ text : "{name}"}),
 						new sap.m.Text({ text : "{productId}"}),
-						new sap.m.Text({ text : "{lastPurchase}"}),
-						/*new sap.m.DatePicker({ 
+//						new sap.m.Text({ text : "{lastPurchase}"}),
+						new sap.m.DatePicker().bindProperty("value", "lastPurchase", function(cellValue){
+//							var oFormat = 
+						}),
+					/*	new sap.m.DatePicker({ 
 							value : {
 								path: '/lastPurchase',
 								type: 'sap.ui.model.type.Date',
 								formatOptions : {
-									source : { pattern : 'yyyy-MM-dd' },
-									pattern: 'yyyy-MM-dd',
-									style : 'full',
-									relative: true,
-									relativeScale: "auto"
+									source : { pattern : 'dd-MM-yyyy' },
+									pattern: 'dd-MM-yyyy',
 								}
-								formatOptions : {
-									source : { pattern : 'yyyy-MM-dd' },
-									pattern: 'yyyy-MM-dd'
-								},
+//								formatOptions : {
+//									source : { pattern : 'yyyy-MM-dd' },
+//									pattern: 'yyyy-MM-dd'
+//								},
 							},
 //							width : "9rem"
 						}),*/
