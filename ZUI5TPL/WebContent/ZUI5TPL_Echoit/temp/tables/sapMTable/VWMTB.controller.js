5b5d56ea8e8666bc0f6431edf983bd19f79f12ef
echoit <echoit@echoit-PC>
2018-02-20 16:54:10 +0900

    sap.m.table page connect test with gateway

--- ZUI5TPL/WebContent/ZUI5TPL_Echoit/temp/tables/sapMTable/VWMTB.controller.js
+++ ZUI5TPL/WebContent/ZUI5TPL_Echoit/temp/tables/sapMTable/VWMTB.controller.js
@@ -3,10 +3,12 @@ sap.ui.define([
 	"com/ui5/echoit/controller/BaseController",
 	"com/ui5/echoit/controller/CommonUtil",
 	"sap/ui/model/odata/ODataModel",
+	"sap/ui/model/Filter",
+	"sap/ui/model/FilterOperator",
 	"sap/ui/core/routing/History",
 	"sap/ui/model/json/JSONModel",
 	"sap/m/MessageToast"
-], function (jQuery, Controller, CommonUtil, ODataModel, History, JSONModel, MessageToast) {
+], function (jQuery, Controller, CommonUtil, ODataModel, Filter, FilterOperator, History, JSONModel, MessageToast) {
 	"use strict";
 
 	return Controller.extend("com.ui5.echoit.temp.tables.sapMTable.VWMTB", {
@@ -19,8 +21,6 @@ sap.ui.define([
 		onRouteMatched : function(oEvent) {
 			var jsonModel = new JSONModel(jQuery.sap.getModulePath("com.ui5.echoit.models", "/customers.json"));
 			this.getView().setModel(jsonModel);
-		
-			var oModel = new ODataModel(CommonUtil.getOdataServiceUrl(), true);
 			
 			var oTable = this.getView().byId("mtable");
 			oTable.setModel(jsonModel);
@@ -61,6 +61,29 @@ sap.ui.define([
 			Controller.prototype.onNavBack.apply(this);
 		},
 		
+		onSearch : function() {
+			var oFilter = [
+				new Filter("ZInput", FilterOperator.EQ, "")
+			];
+			
+			var result = this.getGatewayReadData(oFilter);
+			var outputjson = JSON.parse(result[0].OutputJson);
+		},
+		
+		// Gateway를 호출하는 Function
+		getGatewayReadData : function(oFilter){
+			var oModel = new ODataModel(CommonUtil.getOdataServiceUrl(), true);
+			
+			var sResult;
+			oModel.read("/ZUI5TPL_TESTSet",{
+				filters : oFilter,
+				async	: false,
+				success : function(oData, oResponse) { sResult = oData.results; },
+				error	: function(oError) { sap.m.MessageBox.error(oError.response.body, { title : "Error" }); }
+			});
+			
+			return sResult;
+		}
 	});
 
 });
