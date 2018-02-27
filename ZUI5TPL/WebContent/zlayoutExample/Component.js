59967d8f7294b13218be6b6186a662b088c506ac
OH INTAEK <flytaek@gmail.com>
2018-02-27 15:54:15 +0900

    layout example

--- ZUI5TPL/WebContent/zlayoutExample/Component.js
+++ ZUI5TPL/WebContent/zlayoutExample/Component.js
@@ -2,7 +2,7 @@ sap.ui.define([
    "sap/ui/core/UIComponent"
 ], function (UIComponent) {
    "use strict";
-   return UIComponent.extend("com.ui5.echoit.Component", {
+   return UIComponent.extend("echoit.zuitpl.layoutExample.Component", {
 
 	   metadata: {
 			manifest: "json"
