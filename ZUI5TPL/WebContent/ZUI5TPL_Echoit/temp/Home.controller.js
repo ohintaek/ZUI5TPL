5b9b2d586856e708012b88da30182b90fbd45c28
chungyoon <chungyoon@DESKTOP-HGBL69V>
2018-03-18 23:59:01 +0900

    Main Page update.. Standard Tile.. 18.03.18 pm11:58

--- ZUI5TPL/WebContent/ZUI5TPL_Echoit/temp/Home.controller.js
+++ ZUI5TPL/WebContent/ZUI5TPL_Echoit/temp/Home.controller.js
@@ -1,8 +1,11 @@
 sap.ui.define([
 	"com/ui5/echoit/controller/BaseController",
+	"com/ui5/echoit/controller/CommonUtil",
 	"sap/ui/core/routing/History",
+	"sap/ui/model/Filter",
+	"sap/ui/model/FilterOperator",
 	"sap/m/MessageToast"
-], function (Controller, History, MessageToast) {
+], function (Controller, CommonUtil, History, Filter, FilterOperator, MessageToast) {
 	"use strict";
 
 	return Controller.extend("com.ui5.echoit.temp.Home", {
@@ -12,12 +15,31 @@ sap.ui.define([
 		},
 		
 		onRouteMatched : function(oEvent) {
-			var oSource = oEvent.getSource();
+			this.setTileContentInit();
 		},
 
 		onNavBack: function() {
 			Controller.prototype.onNavBack.apply(this);
 		},
+		
+		// 공지사항의 총 갯수를 구한다.
+		setTileContentInit : function(){
+			var oFilter = [
+				new Filter("ZFlag", FilterOperator.EQ, "NOTICECOUNT")
+			];
+			
+			var selectResult = CommonUtil.getGatewayQueryData(oFilter, "/ZUI5TPL_NOTICESet");
+			if(selectResult[0].EType == "E")
+				return;
+			
+			var oNoticeTile = this.getView().byId("noticeTile");
+			oNoticeTile.setNumber(selectResult[0].NoticeAllCount);
+		},
+		
+		// 공지 사항 화면으로 이동한다.
+		onPressNotice : function(){
+			this.getRouter().navTo("VWNoticeBoard");
+		}
 	});
 
 });
