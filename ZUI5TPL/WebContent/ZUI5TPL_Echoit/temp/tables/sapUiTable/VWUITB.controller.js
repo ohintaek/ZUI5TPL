c25300a3eda7aa3da97521b33deaf596d6422d4c
echoit <echoit@echoit-PC>
2018-07-03 18:14:34 +0900

    panel property

--- ZUI5TPL/WebContent/ZUI5TPL_Echoit/temp/tables/sapUiTable/VWUITB.controller.js
+++ ZUI5TPL/WebContent/ZUI5TPL_Echoit/temp/tables/sapUiTable/VWUITB.controller.js
@@ -20,6 +20,49 @@ sap.ui.define([
 			Controller.prototype.onNavBack.apply(this);
 		},
 		
+		// Panel Background Change
+		onPanelBgChange : function(oEvent){
+			var oCheck = oEvent.getSource();
+			
+			var isCheck = oCheck.getSelected();
+			
+			var sPropName = oCheck.getProperty("text");
+			
+			var oPanel = this.getView().byId("Panel1");
+			if(isCheck){
+				oPanel.setBackgroundDesign(sPropName);
+			} else {
+				oPanel.setBackgroundDesign("Translucent");
+			}
+		},
+		
+		// Panel Expanded
+		onPanelExpended : function(oEvent){
+			var isCheck = oEvent.getSource().getSelected();
+			
+			var oPanel = this.getView().byId("Panel1");
+			
+				oPanel.setExpandable(isCheck);
+				oPanel.setExpandAnimation(isCheck);
+				oPanel.setExpanded(isCheck);
+		},
+		
+		// HBox BackgoundDesign Change
+		onHBoxBgChange : function(oEvent){
+			var oCheck = oEvent.getSource();
+			
+			var isCheck = oCheck.getSelected();
+			
+			var sPropName = oCheck.getProperty("text");
+			
+			var oHBox = this.getView().byId("p2_HBox");
+			if(isCheck){
+				oHBox.setBackgroundDesign(sPropName);
+			} else {
+				oHBox.setBackgroundDesign("Translucent");
+			}
+		}
+		
 	});
 
 });
